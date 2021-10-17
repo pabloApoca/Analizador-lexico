991ba009047130d851b33958e503930a7492bb54
pabloApoca <pablo_mtv08@hotmail.com>
2021-10-17 16:21:14 -0300

    Push del initial scaffolding para la app.

---
+++ src/main/java/com/fytc/AnalizadorLexico/AnalizadorLexicoApplication.java
@@ -0,0 +1,13 @@
+package com.fytc.AnalizadorLexico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalizadorLexicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalizadorLexicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fytc/AnalizadorLexico/AnalizadorLexicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fytc.AnalizadorLexico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalizadorLexicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
