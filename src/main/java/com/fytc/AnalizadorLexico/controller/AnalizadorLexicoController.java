c0b79288e971eac67e86cb9a9e37e67469c6c163
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2021-11-09 00:29:05 -0300

    Avance para leer archivos txt locales.

--- src/main/java/com/fytc/AnalizadorLexico/controller/AnalizadorLexicoController.java
+++ src/main/java/com/fytc/AnalizadorLexico/controller/AnalizadorLexicoController.java
@@ -2,10 +2,9 @@ package com.fytc.AnalizadorLexico.controller;
 
 import com.fytc.AnalizadorLexico.utils.AnalizadorLexicoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -31,5 +30,12 @@ public class AnalizadorLexicoController {
     @PostMapping("/analizartexto")
     public String escribir (@RequestBody String texto) throws IOException {
         return analizadorLexicoService.analizarTexto(texto);
+    }
+
+    //@PostMapping("/analizartxt")
+    @PutMapping ( value = "/analizartxt", consumes = { "multipart/form-data" })
+    public String abrirtxt(@RequestPart ( "imageFile") MultipartFile imageFile ) throws IOException {
+
+        return analizadorLexicoService.analizarTxt(imageFile);
     }
 }
--- src/main/java/com/fytc/AnalizadorLexico/utils/AnalizadorLexicoService.java
+++ src/main/java/com/fytc/AnalizadorLexico/utils/AnalizadorLexicoService.java
@@ -1,6 +1,7 @@
 package com.fytc.AnalizadorLexico.utils;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 
@@ -55,5 +56,50 @@ public class AnalizadorLexicoService {
         }
     }
 
+
+    public String analizarTxt(MultipartFile txt) throws IOException {
+        File archivo = new File("archivo.txt");
+        PrintWriter escribir;
+
+        MultipartFile newTxt = null;
+
+        newTxt.transferTo((File) txt);
+
+
+        try {
+            escribir = new PrintWriter(archivo);
+            escribir.print(newTxt);
+            escribir.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Reader lector = new BufferedReader(new FileReader("archivo.txt"));
+        Lexer lexer = new Lexer(lector);
+
+        String resultado = "";
+        String resultadoFinal = "";
+
+        while (true){
+            Tokens tokens = lexer.yylex();
+            if (tokens==null){
+                resultado += "FIN";
+                resultadoFinal += resultado;
+                return resultadoFinal;
+            }
+            switch (tokens) {
+                case ERROR:
+                    resultado += lexer.yytext() + " ERROR: Simbolo no definido.\n";
+                    break;
+                case Identificador: case Numero:
+                    resultado += lexer.lexeme + " --> " + tokens + "\n";
+                    break;
+                default:
+                    //resultado += "Token: " + tokens + "\n";
+                    resultado += lexer.yytext() + " --> "+ tokens + "\n";
+                    break;
+            }
+        }
+    }
 
 }
